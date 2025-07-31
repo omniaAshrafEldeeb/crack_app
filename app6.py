925842db121eb705c80b278c4530b9e6a80785db
omniaAshrafEldeeb <rannarroto@gmail.com>
2025-07-31 20:55:39 +0300

    Update app6.py

--- app6.py
+++ app6.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 # Sidebar navigation
 page = st.sidebar.selectbox("Choose Page", ["Crack Detection & Repair Tips", "About"])
-st.sidebar.image(image, caption="My Image",use_container_width=True)
+st.sidebar.image(image, caption="Built with ❤️ by Our Team",use_container_width=True)
 
 if page == "Crack Detection & Repair Tips":
 
